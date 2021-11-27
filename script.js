9a4035f44b504bbc6f33139a3c843db54e3f29c8
Mary Margaret <mmlawton15@gmail.com>
2021-11-26 19:27:08 -0500

    added array of questions and answers, I'm sure theres an easier way to do this

--- script.js
+++ script.js
@@ -14,6 +14,76 @@
 
 
 
+//ARRAY OF QUESTIONS
+var questions = [
+    {
+        question: "Arrays in Javascript can be used to store ___?",
+        answers: ("Booleans", "Numbers and Strings", "Other Arrays", "All of the above"),
+        correctAnswer: "All of the Above",
+    },
+    {   question: "Commonly used Data Types include ___.",
+        answers: ("Booleans", "Strings", "Arrays", "All of the Above"),
+        correctAnswer: "All of the Above",
+    },
+    {
+        question: "The condidtion in an if/else statement is enclosed with a ___",
+        answers: ("Bracket", "Curly Brace", "Parenthesis", "All of the above"),
+        correctAnswer: "Parenthesis"
+    },
+    {
+        question: "A very useful tool used during development and debugging",
+        answers: ("Google", "For Loops", "Debugger", "all of the above"),
+        correctAnswer: "All of the above",
+    },
+    {
+        question: "String values must be enclosed with ___ when being assigned to variables.",
+        answers: ("curly braces", "square brackets", "parentheses", "curly braces and parentheses"),
+        correctAnswer: "Curly braces",
+    },
+    {
+        question: "DOM stands for ___?",
+        answers: ("Direct Optimized Model", "document Object Model", "Document Oblivion Matches", "Diligent Obtuse Monkey"),
+        correctAnswer: "Document Object Model"
+    },
+    {
+        question: "What is the difference between Javascript and HTML?",
+        answers: ("Javascript is the primary structure of a website, HTML adds dynamic content to the code", "HTML provides the primary structure of a website, while Javascript adds dynamic content to that HTML code", "HTML is ugly and javascript is pretty", "Javascript is easy and HTML is difficult"),
+        correctAnswer: "HTML provides the primary structure of a website, while Javascript adds dynamic content to that HTML code"
+    },
+    {
+        question: "CSS stands for ___?",
+        answers: ("Cartridge Section Styler", "Colorful Sheet Styler", "Cascading Style Sheets", "Card structure stylizer"),
+        correctAnswer: "Cascading Style Sheets",
+    },
+    {
+        question: "Which of the following are NOT forms of the command line?",
+        answers: ("Kanban", "bash", "terminal", "all of the above"),
+        correctAnswer: "Kanban",
+    },
+    {
+        question: "Who created the World Wide Web?",
+        answers: "Tim Berners-Lee, Steve Jobs, Elon Musk, Benjamin Franklin",
+        correctAnswer: "Tim Berners-Lee",
+    },
+];
+var score = 0;
+
+
+
+
+//LOOP FOR QUESTIONS
+for (var i=0; i <questions.length; i++){
+    var answers = window.prompt(questions[i].prompt);
+    if(response == questions[i].answer){
+        score++;
+        alert("Correct");
+    } else {
+        alert("Incorrect");
+        //deduct time here
+    }
+}
+alert("You got " + score + "of " + questions.length);
+
 
 
 // TIMER CODE
